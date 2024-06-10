7b36622bccffb9e8ecd5216810627456de3ef1d6
Retrx <168238206+OriginallyRetro@users.noreply.github.com>
2024-06-10 15:05:40 -0400

    Update Untitled TBFG.py

--- Untitled TBFG.py
+++ Untitled TBFG.py
@@ -47,16 +47,17 @@ storyExperience = 0
 #---------------------------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------------
 class defaultCharacter:
-    def __init__(player, name, Xp: int, level: int, attack: int, health: int, light_level: int, starting_point: int) -> None:
+    def __init__(player, name, Xp: int, level: int, attack: int, health: int, defense: int, light_level: int, startingFight: int) -> None:
         player.name = name
         player.Xp = Xp
         player.level = level
         player.attack = attack
         player.health = health
+        player.defense = defense
         player.light_level = light_level
-        player.starting_point = starting_point
+        player.startingFight = startingFight
         
-Player = defaultCharacter(name = userName, Xp = 0, level = 0, attack = 30, health = 750, light_level = 0, starting_point = 0)
+Player = defaultCharacter(name = {userName}, Xp = 0, level = 0, attack = 30, health = 750, defense = 510, light_level = 0, startingFight = 0)
 
 
 #---------------------------------------------------------------------------------------------------------
@@ -96,12 +97,14 @@ class windBoss:
         windBoss.attack = attack
         windBoss.health = health
 class windMinion:
-    def __init__(windMinion, name: str, attack: int, health:int) -> None:
+    def __init__(windMinion, name: str, attack: int, health: int, defense: int) -> None:
+        windMinion.name = name
         windMinion.attack = attack
         windMinion.health = health
+        windMinion.defense = defense
 
 
-firstWindSpirit = windMinion(name = "Wind Spirit", attack = 10, health = 150)
+firstWindSpirit = windMinion(name = "Wind Spirit", attack = 10, health = 150, defense = 60)
 #---------------------------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------------
 
@@ -175,64 +178,86 @@ def tutorialMode():
 #--------------------------------------------------------------------------------------------------------------------
 
 
-
+global story_Point
+story_Point = 0
 
 def storyMode():
-    if Player.starting_point <= 0:
+    if story_Point == 0:
         os.system("cls")
-        fast_print('     Narrator: "Within the foreboding depths of the Evil Spirit Dungeon, darkness reigns supreme. Its twisted corridors are a ghastly tapestry of jagged stone and flickering torchlight, casting eerie shadows that seem to dance with malevolent intent.\nTraps lie in wait, eager to ensnare the unwary, while spectral entities haunt every corner, their mournful wails a constant reminder of the horrors that dwell within. Yet amidst the peril, there are treasures to be found and challenges to be overcome, for those brave or foolish enough to dare the dungeons depths.\nBut beware, for in the heart of darkness, even the most valiant souls may find themselves consumed by the abyss. Prepare you heart for the unknown light mage...\n\n\n"     ')
+        fast_print('     Narrator: "Within the foreboding depths of the Elemental Spirit Dungeon, darkness reigns supreme. Its twisted corridors are a ghastly tapestry of jagged stone and flickering torchlight, casting eerie shadows that seem to dance with malevolent intent.\nTraps lie in wait, eager to ensnare the unwary, while spectral entities haunt every corner, their mournful wails a constant reminder of the horrors that dwell within. Yet amidst the peril, there are treasures to be found and challenges to be overcome, for those brave or foolish enough to dare the dungeons depths.\nBut beware, for in the heart of darkness, even the most valiant souls may find themselves consumed by the abyss. Prepare for the unknown and prepare your heart, light mage..."\n\n\n     ')
+        input()
         os.system("cls")
-        delay_print('  "???:\n"Hey! Get up you alright, man?"   ')
+        delay_print('  ???:\n"Hey! Get up, you alright, man?"   ')
         input('\n')
-        delay_print('   "You:\n"Whats going on, Where am I?"  ')
+        delay_print('  You:\n"Whats going on, Where am I?"  ')
         input('\n')
         delay_print('  ???:\n "Thats not important, you got knocked out by a spirit, right now lets get through this dungeon!" ')
         input('\n')
-        delay_print('   "(Thinking to yourself): So, he ignored my question then answered it? This guy is a weirdo. Wait, a dungeon?!?! And a spirit too?"  ')
+        delay_print('  "(Thinking to yourself): So, he ignored my question then answered it? This guy is a weirdo. Wait, a dungeon?!?! And a spirit too?"  ')
         input('\n')
-        fast_print('  ???:\n"Look a wind spirit, attack fend for you life!')
+        fast_print('   ???:\n"Look a wind spirit, attack fend for you life!"  ')
         input()
         os.system("cls")
-        input("Press ENTER get ready for battle!\n\n***Once you hit enter you will attack***")
+        input("Press ENTER get ready for battle!")
         os.system("cls")
+    elif story_Point > 0:
+        fast_print('   Narrator: "Emerging from the depths of the evil elemental spirit dungeon, I breathe a sigh of relief, my heart ablaze with triumphant victory. The burden of darkness lifts, but a flicker of concern remains for the lingering shadows left behind. As I step into the light outside of the dungeon, victorious yet vigilant, the need for tranquillity resonates within..."   ')
+        input('\n')
+        
+    while True:
 
-        while True:
+        def fight(Player, firstWindSpirit)-> None:
 
-            def firstFight (defaultCharacter, target) -> None:
+            if firstWindSpirit.defense >= 0:
+                firstWindSpirit.defense -= Player.attack
+                
+        ### On the other hand if it is less than or equal to zero it'll attack the persons health
+            if firstWindSpirit.defense <= 0:
+                firstWindSpirit.defense = 0
+                firstWindSpirit.health -= Player.attack
 
-                print(f"You attacked the wind spirit! dealt 30 DAMAGE!\n")
-                target.health -= defaultCharacter.attack
-                print(f"Wind Spirits health: {firstWindSpirit.health}\n------------\n")
-                defaultCharacter.health -= target.attack
-                print("Wind spirit dealt 10 DAMAGE!\n")
-                print(f"Your health: {Player.health}")
+            if Player.defense >= 0:
+                Player.defense -= firstWindSpirit.attack
 
-            firstFight(Player, firstWindSpirit)
+            if Player.defense <= 0:
+                Player.defense = 0
+                Player.health -= firstWindSpirit.attack
+            #This is to begin so when you press enter it doesnt automatticaly start the fight
+            if Player.startingFight <= 0:
+                print("----------------------------------------------------------------")
+                print(f"Wind Spirit health: {firstWindSpirit.health},\n{userName} health: {Player.health}\n----------\n")
+                print(f"Wind Spirit defense: {firstWindSpirit.defense},\n{userName} defense: {Player.defense}")
+                Player.startingFight += 1
+            elif Player.startingFight >= 0:
+                print("----------------------------------------------------------------")
+                print(f"Wind spirit has dealt 10 DAMAGE!\n{userName} has dealt 30 DAMAGE!")
+                print("----------------------------------------------------------------------")
+                print(f"Wind Spirit health: {firstWindSpirit.health},\nWind Spirit defense: {firstWindSpirit.defense}\n----------\n")
+                print(f"{userName} health: {Player.health},\n{userName} defense: {Player.defense}")
 
             if firstWindSpirit.health < 0:
-                os.system("cls")
+                print("Wind spirit has died")
+                input(firstAfterMath())
 
-                firstAfterMath()
-
-            input("")
-            os.system("cls")
-
-    if starting_point > 0:
-        delay_print(' ???: "Come on lets blow this stand, we gotta get out of the dungeon!')
+        fight(Player, firstWindSpirit)
+    
         input()
-        fast_print("Narrator:\nSo as our two young heros fend for their lives against the wind Spirit.\n They leave in fear, not wanting to test their luck for another day.") 
 
-
+    
 def firstAfterMath():
+        os.system("cls")
         global Xp, level, storyExperience
         Xp = 0
-        level = 0 #Try you beat the {target} with an f string perhapd?
+        story_point = 0
+        level = 0
+        storyExperience = 0
         afterMath_print("You beat the WINDSPIRIT!")
         Xp += 100
         level += 1
         afterMath_print(f"Level up! {level} Total XP: {Xp} Items gained: None\n")
         afterMath_print(f"You beat the Wind Spirit!")
-        storyExperience += 1
+        story_point += 1
+        input()
         mainMenu()
 
 
